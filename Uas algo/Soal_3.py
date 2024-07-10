9decff4668fd86b7daea89387e12ed3c3cdb7723
SalsabilaRmdni <165672889+SalsabilaRmdni@users.noreply.github.com>
2024-07-10 12:25:18 +0700

    add uas

---
+++ Uas algo/Soal_3.py
@@ -0,0 +1,18 @@
+class Queue:
+    def init(self):
+        self.queue = []
+
+    def enqueue(self, order):
+        self.queue.append(order)
+        print(f"Order '{order}' di tambahkan.")
+
+    def dequeue(self):
+        if len(self.queue) < 1:
+            return "Queue is empty"
+        order = self.queue.pop(0)
+        print(f"Order '{order}' dihapus.")
+        return order
+
+    def tampilan(self):
+        print(f"Antrian saat ini: {self.queue}")
+        
---
+++ Uas algo/Soal_4.py
@@ -0,0 +1,39 @@
+class Buah:
+    def init(self, nama, warna, rasa):
+        self.nama = nama
+        self.warna = warna
+        self.rasa = rasa
+    
+    def setNama(self, nama):
+        self.nama = nama
+    
+    def setWarna(self, warna):
+        self.warna = warna
+    
+    def setRasa(self, rasa):
+        self.rasa = rasa
+    
+    def information(self):
+        return f"Nama: {self.nama}, Warna: {self.warna}, Rasa: {self.rasa}"
+
+
+class Mangga(Buah):
+    def init(self, nama, warna, rasa, vitamin):
+        super().init(nama, warna, rasa)
+        self.vitamin = vitamin
+    
+    def setVitamin(self, vitamin):
+        self.vitamin = vitamin
+    
+    def setInformation(self):
+        info = super().information()
+        return f"{info}, Vitamin: {self.vitamin}"
+
+
+mangga1 = Mangga("Mangga Arumanis", "Hijau", "Manis", "Vitamin C")
+
+
+print(mangga1.information())      
+print(mangga1.setInformation())   
+mangga1.setVitamin("Vitamin A")
+print(mangga1.setInformation())
